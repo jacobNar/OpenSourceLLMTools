1f69735c33185dd48735918821c4230aba73442b
jacobNar <jdnarayan56@gmail.com>
2025-11-15 00:46:07 -0600

    fix browser persistence issues. agent flow/prompts still need improviing

--- chrome-dev-tools-demo/main.py
+++ chrome-dev-tools-demo/main.py
@@ -13,6 +13,7 @@ from langchain_core.messages import ToolMessage
 from langchain_core.runnables import RunnableConfig
 from langgraph.graph.message import AnyMessage
 from langgraph.prebuilt import ToolNode
+from langchain_mcp_adapters.tools import load_mcp_tools
 # The 'chat' object is your ChatOllama instance bound with tools
 
 
@@ -33,7 +34,7 @@ def call_model(state: AgentState, config: RunnableConfig) -> dict:
 async def call_tool(state: AgentState, config: RunnableConfig) -> dict:
     ai_message = state['messages'][-1]
     tool_calls = ai_message.tool_calls
-    tool_executor = ToolNode(tools)
+
     single_tool_call = [tool_calls[0]]
     tool_messages = await tool_executor.ainvoke(single_tool_call, config=config)
     print("result from tool")
@@ -55,7 +56,7 @@ def should_continue(state: AgentState) -> str:
 
 
 async def main():
-    global tools, chat, agent
+    global tools, chat, agent, tool_executor
     client = MultiServerMCPClient(
         {
             "chrome-devtools": {
@@ -65,87 +66,56 @@ async def main():
                 "args": [
                     "chrome-devtools-mcp@latest",
                     "--headless=false"
-                    "--isolated=true"
                 ]
             }
         }
     )
-    tools = await client.get_tools()
-    # desired_tool_names = [
-    #     "new_page",
-    #     "navigate_page"
-    #     "take_snapshot"
-    #     "click",
-    #     "fill_form",
-    #     "fill",
-    #     "wait_for"
-    # ]
+    async with client.session("chrome-devtools") as session:
+        tools = await load_mcp_tools(session)
+        tool_executor = ToolNode(tools)
+        # print(tools)
+        print(len(tools), "tools loaded.")
 
-    # tools = [
-    #     tool for tool in tools
-    #     if tool.name in desired_tool_names
-    # ]
+        chat = ChatOllama(
+            base_url="http://localhost:11434/",
+            model="qwen3:latest",
+            temperature=0.8,
+            num_predict=40000,  # Increased for 128k context window
+        ).bind_tools(tools)
 
-    # print(tools)
-    print(len(tools), "tools loaded.")
+        agent = create_agent(
+            chat,
+            tools,
+            system_prompt="your are an expert at using tools to browse the web. after each tool call replan and make sure that the users requests are fully satisfied.",
+        )
+        workflow = StateGraph(AgentState)
 
-    chat = ChatOllama(
-        base_url="http://localhost:11434/",
-        model="qwen3:latest",
-        temperature=0.8,
-        num_predict=40000,  # Increased for 128k context window
-    ).bind_tools(tools)
+        workflow.add_node("agent", call_model)
+        workflow.add_node("tool", call_tool)
 
-    agent = create_agent(
-        chat,
-        tools,
-        system_prompt="your are an expert at using tools to browse the web. Keep the browser tab open."
-    )
+        workflow.set_entry_point("agent")
 
-    # # Stream the agent steps as they happen
-    # print("Streaming agent steps:")
-    # async for step in agent.astream({
-    #     "messages": [HumanMessage(
-    #         content="use your tools to 1. visit https://promo.united.com/offers/packmoremiles. 2. fill out the form using xkc61520 as the mileageplus number and any number for last 4 of credit card. 3. submit the form. 4. wait for the website to finish loading. 5. verify the form submission by reading the text on the page.")]
-    # }):
-    #     print(step)
+        workflow.add_conditional_edges(
+            "agent",
+            should_continue,
+            {
+                "continue": "tool",
+                "end": END
+            }
+        )
 
-    #     print('\n---------------------------------\n')
-    workflow = StateGraph(AgentState)
+        workflow.add_edge('tool', 'agent')
 
-    # 2. Add Nodes
-    workflow.add_node("agent", call_model)
-    workflow.add_node("tool", call_tool)
+        app = workflow.compile(checkpointer=MemorySaver())
+        config = {"configurable": {"thread_id": "3"}}
 
-    # 3. Set Entry Point
-    workflow.set_entry_point("agent")
-
-    # 4. Define Edges (The Flow)
-
-    # From Agent node, use the conditional router to decide the next step
-    workflow.add_conditional_edges(
-        "agent",  # Source node
-        should_continue,  # Router function
-        {
-            "continue": "tool",  # If tool call exists, go to tool execution
-            "end": END           # If final answer, end the graph
-        }
-    )
-
-    # From Tool node, ALWAYS go back to the Agent for the next decision/thought
-    workflow.add_edge('tool', 'agent')
-
-    # 5. Compile the app
-    app = workflow.compile(checkpointer=MemorySaver())
-    config = {"configurable": {"thread_id": "1"}}
-    # --- Invocation ---
-    print("Streaming agent steps:")
-    async for step in app.astream(
-        {"messages": [HumanMessage(
-            content="use your tools to visit https://promo.united.com/offers/packmoremiles. Once there fill out the form with the account xkc1520 and the card number 1234. hit submit. Then tell me what the page says after submission.")]
-         }, config=config):
-        print(step)
-        print('\n---------------------------------\n')
+        print("Streaming agent steps:")
+        async for step in app.astream(
+            {"messages": [HumanMessage(
+                content="use your tools to visit https://promo.united.com/offers/packmoremiles. Once there fill out the form with the account xkc1520 and the card number 1234 and submit it.")]
+             }, config=config):
+            print(step)
+            print('\n---------------------------------\n')
 
 if __name__ == "__main__":
     asyncio.run(main())
